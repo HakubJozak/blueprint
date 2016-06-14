222f7a15b7745a21be03b19053040ecb283c2655
Jakub Hozak <jakub.hozak@gmail.com>
2016-06-14 15:33:07 +0200

    + delete buttons and icons

---
+++ app/helpers/blueprint/buttons_helper.rb
@@ -0,0 +1,20 @@
+module Blueprint::ButtonsHelper
+
+  def create_button(text,url)
+    title = "#{icon('plus')} #{text}".html_safe
+    link_to(title, url, class: 'btn btn-success')
+  end
+  
+  def delete_button(record, path, opts = { label: 'Smazat' })
+    name = record.try(:full_title) || record.try(:title) || record.try(:to_label)
+    question = t('really_delete',title: name)
+    ico = icon 'trash'
+
+    opts.reverse_merge!('data-confirm': question,
+                        method: :delete,
+                        class: 'btn btn-default pull-right')
+
+    link_to( ico, path, opts).html_safe
+  end  
+
+end
---
+++ app/helpers/blueprint/icon_helper.rb
@@ -0,0 +1,12 @@
+module Blueprint::IconHelper
+
+  def icon(name, title = nil)
+    i = "<i class='fa fa-#{name}'></i>"
+    [ i, title ].compact.join(' ').html_safe
+  end
+
+  def icon_with_tooltip(name, title, tooltip)
+    %(<i class="fa fa-#{name} popover-me" title="#{title}" data-content="#{tooltip}"></i>).html_safe
+  end
+  
+end
