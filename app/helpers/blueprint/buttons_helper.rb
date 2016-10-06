dcbc01a52b46c6b48a1fb87dfcfe857059200a33
Jakub Hozak <jakub.hozak@gmail.com>
2016-10-06 13:41:30 +0200

    * flexible label for delete button

--- app/helpers/blueprint/buttons_helper.rb
+++ app/helpers/blueprint/buttons_helper.rb
@@ -4,17 +4,17 @@ module Blueprint::ButtonsHelper
     title = "#{icon('plus')} #{text}".html_safe
     link_to(title, url, class: 'btn btn-success')
   end
-  
-  def delete_button(record, path, opts = { label: 'Smazat' })
+
+  def delete_button(record, path, opts = {})
     name = record.try(:full_title) || record.try(:title) || record.try(:to_label)
     question = t('really_delete',title: name)
-    ico = icon 'trash'
+    
 
     opts.reverse_merge!('data-confirm' => question,
                         method: :delete,
                         class: 'btn btn-default pull-right')
-
-    link_to( ico, path, opts).html_safe
-  end  
+    label = opts.delete(:label) ||  icon('trash')
+    link_to( label, path, opts).html_safe
+  end
 
 end
